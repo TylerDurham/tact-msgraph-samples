f4561eef5c511119a624077a45b86fd50118ed3d
Tyler Durham <TylerDurham@users.noreply.github.com>
2024-10-25 21:39:35 -0500

    Initial commit.

--- msgraph-spa/src/index.tsx
+++ msgraph-spa/src/index.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import { PublicClientApplication } from '@azure/msal-browser';
-import { MsalProvider } from '@azure/msal-react';
-import { msalConfig } from './authConfig';
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./authConfig";
+console.log("test");
+console.log(process.env);
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <MsalProvider instance={msalInstance}>
-          <App />
-      </MsalProvider>
-  </React.StrictMode>
+      <App />
+    </MsalProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
